365cbde507e558de532116c23ff67c129ec0f91a
TejasSatish <tejassatish@gmail.com>
2023-05-11 22:21:57 +0530

    CRD done - need to add U - need to create more R methods

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require(`express`)
 const cors = require(`cors`) 
 const mongoose = require(`mongoose`)
+var ObjId = require('mongoose').mongo.BSON.ObjectId;
 const Blog = require('./blogModel')
 const app = express()
 
@@ -23,7 +24,7 @@ mongoose.connect('mongodb+srv://tejassatish:mernstack@cluster0.zh6szkp.mongodb.n
 app.post(`/blogs`,async (req,res)=>{
     try{
         const blog = await Blog.create(req.body)
-        res.status(200).json(blog)
+        return res.status(200).json(blog)
     }catch(err){
         console.log(err)
     }
@@ -36,6 +37,23 @@ app.get(`/blogs`, async (req,res)=>{
     }catch(e){
         console.error(e) 
         console.log(typeof(db))
+    }
+})
+
+app.delete(`/blogs/:id`, async (req,res)=>{
+    try{
+        //let id= new mongoose.mongo.BSON.ObjectId(req.body)
+        //const blogs = await Blog.findByIdAndDelete(req.body.id)
+        console.log(req.params)
+        let {id}= req.params
+        const blog= await Blog.findByIdAndDelete(id);
+        if(!blog){
+            return res.status(404).json({"error": "no such id"})
+        }
+
+        return res.status(200).json({"success":"true"})
+    }catch(err){
+        console.log(err)
     }
 })
 
